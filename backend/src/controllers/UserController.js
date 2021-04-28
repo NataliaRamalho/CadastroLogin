e49566178b6e4437b1e28f6d9654c407c767e7c6
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2021-04-28 15:28:11 -0300

    Configuração dos métodos de cadastro e login

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,15 +1,34 @@
+const bcrypt = require('bcrypt-nodejs')
 const User = require('../models/User');
 
+const encryptPassword = password =>{
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password,salt);
+}
+
 module.exports ={
+
     async register(req, res){
         const {email, name, password} = req.body;
-        const user = await User.create({name, email, password})
-        return res.json(user)
+        const userExist = await User.findOne({where: {email}})
+        if(userExist){
+            return res.status(400).send('Usuario ja cadastrado')
+        }
+        const newPassword = encryptPassword(password)
+        const user = await User.create({name, email, password:newPassword})
+        return res.status(201).json(user)
     },
 
     async login(req, res){
         const {email, password} = req.body;
-        const user = await User.findOne({where: {email, password}, attributes: ['id','email','name']});
-        return res.json(user)
+        const user = await User.findOne({where:{email}})
+        if(!user){
+            return res.status(404).send('Email invalido')
+        }
+        const isMatch = bcrypt.compareSync(password, user.password)
+        if(!isMatch){
+            return res.status(404).send('Senha invalida')
+        }        
+        return res.status(200).json({'email': user.email, 'name':user.name})
     }
 }
